83689068dd5c7a7cb18fd58852968a811b4e020b
HP <g00433839@atu.ie>
2025-09-30 21:31:07 +0100

    Fixed few bugs

--- src/main/java/atu/ie/lab1/Person.java
+++ src/main/java/atu/ie/lab1/Person.java
@@ -4,18 +4,31 @@ public class Person {
     private String name;
     private int age;
 
+    // Default constructor (important for JSON serialization/deserialization)
+    public Person() {
+    }
+
     public Person(String name, int age) {
         this.name = name;
         this.age = age;
+    }
+
+    // Getters
+    public String getName() {
+        return name;
     }
 
     public int getAge() {
         return age;
     }
 
-    public String getName() {
-        return name;
+    // Setters (needed for Spring to map JSON -> Object if you accept Person in requests)
+    public void setName(String name) {
+        this.name = name;
     }
 
-
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
+
